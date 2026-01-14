546181a458b6f352c6bcdaeb6423298fdfc3a6d0
Patryk Andrzejewski <vox3r69@gmail.com>
2026-01-14 23:55:41 +0100

    fix tests

--- packages/example/src/modules/example/vector.ts
+++
@@ -1,7 +0,0 @@
-import type { VectorModuleConfig } from '@open-mercato/shared/modules/vector'
-
-export const vectorConfig: VectorModuleConfig | null = null
-
-export const config = vectorConfig
-
-export default vectorConfig
