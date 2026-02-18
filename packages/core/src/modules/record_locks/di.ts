c1fc28e3bc74ed984fc8e8fd7eb6569f7461d4f0
Mateusz Soroka <xmsoroka@gmail.com>
2026-02-18 08:29:10 +0100

    fix(record_locks): register recordLockService with classic DI args

--- packages/core/src/modules/record_locks/di.ts
+++ packages/core/src/modules/record_locks/di.ts
@@ -4,14 +4,9 @@ import type { AppContainer } from '@open-mercato/shared/lib/di/container'
 import type { ModuleConfigService } from '@open-mercato/core/modules/configs/lib/module-config-service'
 import { createRecordLockService } from './lib/recordLockService'
 
-type RecordLockCradle = AppContainer['cradle'] & {
-  em: EntityManager
-  moduleConfigService?: ModuleConfigService | null
-}
-
 export function register(container: AppContainer) {
   container.register({
-    recordLockService: asFunction(({ em, moduleConfigService }: RecordLockCradle) =>
+    recordLockService: asFunction((em: EntityManager, moduleConfigService?: ModuleConfigService | null) =>
       createRecordLockService({ em, moduleConfigService: moduleConfigService ?? null }),
     ).scoped(),
   })
