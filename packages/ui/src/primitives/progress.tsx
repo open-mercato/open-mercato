0e4acc0f996ee192344888410946122062fbbd40
Chabros Piotr <piotrekchabros@gmail.com>
2026-02-20 23:26:24 +0100

    Remove workflow event triggers migration and update progress component import
    - Delete Migration20260220214819 that created workflow_event_triggers table
    - Update progress.tsx to import cn utility from shared package instead of local lib

--- packages/ui/src/primitives/progress.tsx
+++ packages/ui/src/primitives/progress.tsx
@@ -1,6 +1,6 @@
 "use client"
 import * as React from 'react'
-import { cn } from '../lib/utils'
+import { cn } from '@open-mercato/shared/lib/utils'
 
 export interface ProgressProps extends React.HTMLAttributes<HTMLDivElement> {
   value: number
