61324dc6bad047e57be959ba0a5752d72e7fb395
Piotr Karwatka <piotr.karwatka@gmail.com>
2026-02-22 07:17:07 +0100

    fix: migration fix

--- packages/core/src/modules/catalog/migrations/Migration20260220164228.ts
+++ packages/core/src/modules/catalog/migrations/Migration20260220164228.ts
@@ -3,11 +3,11 @@ import { Migration } from '@mikro-orm/migrations';
 export class Migration20260220164228 extends Migration {
 
   override async up(): Promise<void> {
-    this.addSql(`alter table "catalog_product_offers" drop column "localized_content";`);
+    this.addSql(`alter table "catalog_product_offers" drop column if exists "localized_content";`);
   }
 
   override async down(): Promise<void> {
-    this.addSql(`alter table "catalog_product_offers" add column "localized_content" jsonb null;`);
+    this.addSql(`alter table "catalog_product_offers" add column if not exists "localized_content" jsonb null;`);
   }
 
 }
