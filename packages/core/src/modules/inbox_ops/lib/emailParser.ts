3f7d1d792580ff025e1c806f3b7acbb5676c8abf
Piotr Karwatka <piotr.karwatka@gmail.com>
2026-02-26 11:36:54 +0100

    fix: codeQL fixes

--- packages/core/src/modules/inbox_ops/lib/emailParser.ts
+++ packages/core/src/modules/inbox_ops/lib/emailParser.ts
@@ -1,4 +1,5 @@
 import { createHash } from 'node:crypto'
+import sanitizeHtml from 'sanitize-html'
 import type { InboxEmail, ThreadMessage } from '../data/entities'
 
 export interface ParsedEmail {
@@ -82,21 +83,14 @@ function stripQuotedReplies(text: string): string {
 }
 
 function stripHtml(html: string): string {
-  return html
-    .replace(/<style[^>]*>[\s\S]*?<\/style>/gi, '')
-    .replace(/<script[^>]*>[\s\S]*?<\/script>/gi, '')
-    .replace(/<br\s*\/?>/gi, '\n')
-    .replace(/<\/p>/gi, '\n\n')
-    .replace(/<\/div>/gi, '\n')
-    .replace(/<\/tr>/gi, '\n')
-    .replace(/<\/li>/gi, '\n')
-    .replace(/<[^>]+>/g, '')
-    .replace(/&nbsp;/gi, ' ')
-    .replace(/&amp;/gi, '&')
-    .replace(/&lt;/gi, '<')
-    .replace(/&gt;/gi, '>')
-    .replace(/&quot;/gi, '"')
-    .replace(/&#39;/gi, "'")
+  const sanitized = sanitizeHtml(html, {
+    allowedTags: [],
+    allowedAttributes: {},
+  })
+
+  return sanitized
+    .replace(/\r\n/g, '\n')
+    .replace(/\r/g, '\n')
     .replace(/\n{3,}/g, '\n\n')
     .trim()
 }
