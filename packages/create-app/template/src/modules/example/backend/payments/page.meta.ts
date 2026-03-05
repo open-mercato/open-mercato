6ad83bf831451396174509c6c832b53f08df48e4
pkarw <piotr@catchthetornado.com>
2026-03-05 14:40:47 +0100

    fix: filling the gaps

---
+++ packages/create-app/template/src/modules/example/backend/payments/page.meta.ts
@@ -0,0 +1,9 @@
+export const metadata = {
+  requireAuth: true,
+  requireFeatures: ['example.backend'],
+  pageTitle: 'Payment Gateway Demo',
+  pageTitleKey: 'example.payments.title',
+  pageGroup: 'Example',
+  pageGroupKey: 'example.nav.group',
+  pageOrder: 20010,
+}
