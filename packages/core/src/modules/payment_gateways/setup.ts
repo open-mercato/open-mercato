404746e8375868903e2bd241563005a8920cacb4
Piotr Karwatka <piotr.karwatka@gmail.com>
2026-03-05 17:00:48 +0100

    fix: ACL fixes

--- packages/core/src/modules/payment_gateways/setup.ts
+++ packages/core/src/modules/payment_gateways/setup.ts
@@ -2,8 +2,8 @@ import type { ModuleSetupConfig } from '@open-mercato/shared/modules/setup'
 
 export const setup: ModuleSetupConfig = {
   defaultRoleFeatures: {
-    superadmin: ['payment_gateways.view', 'payment_gateways.manage', 'payment_gateways.capture', 'payment_gateways.refund'],
-    admin: ['payment_gateways.view', 'payment_gateways.manage', 'payment_gateways.capture', 'payment_gateways.refund'],
+    superadmin: ['payment_gateways.*', 'payment_gateways.view', 'payment_gateways.manage', 'payment_gateways.capture', 'payment_gateways.refund'],
+    admin: ['payment_gateways.*', 'payment_gateways.view', 'payment_gateways.manage', 'payment_gateways.capture', 'payment_gateways.refund'],
     employee: ['payment_gateways.view'],
   },
 }
