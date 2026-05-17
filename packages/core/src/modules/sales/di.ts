a3399ea04d28cb5bd4b75cb6d79e58b56ccd4a43
Krzysztof Polak <kriss145@gmail.com>
2026-05-17 21:30:57 +0200

    feat(sales): add additional sales entities to the dependency injection container

--- packages/core/src/modules/sales/di.ts
+++ packages/core/src/modules/sales/di.ts
@@ -5,7 +5,25 @@ import type { AppContainer } from '@open-mercato/shared/lib/di/container'
 import { DefaultSalesCalculationService } from './services/salesCalculationService'
 import { DefaultTaxCalculationService } from './services/taxCalculationService'
 import { SalesDocumentNumberGenerator } from './services/salesDocumentNumberGenerator'
-import { SalesOrder, SalesChannel, SalesShipment } from './data/entities'
+import {
+  SalesOrder,
+  SalesQuote,
+  SalesChannel,
+  SalesShipment,
+  SalesInvoice,
+  SalesCreditMemo,
+  SalesPayment,
+  SalesReturn,
+  SalesOrderLine,
+  SalesQuoteLine,
+  SalesNote,
+  SalesDocumentAddress,
+  SalesDocumentTag,
+  SalesShippingMethod,
+  SalesDeliveryWindow,
+  SalesPaymentMethod,
+  SalesTaxRate,
+} from './data/entities'
 
 type AppCradle = AppContainer['cradle'] & {
   em: EntityManager
@@ -30,7 +48,21 @@ export function register(container: AppContainer) {
       .singleton()
       .proxy(),
     SalesOrder: asValue(SalesOrder),
+    SalesQuote: asValue(SalesQuote),
     SalesChannel: asValue(SalesChannel),
     SalesShipment: asValue(SalesShipment),
+    SalesInvoice: asValue(SalesInvoice),
+    SalesCreditMemo: asValue(SalesCreditMemo),
+    SalesPayment: asValue(SalesPayment),
+    SalesReturn: asValue(SalesReturn),
+    SalesOrderLine: asValue(SalesOrderLine),
+    SalesQuoteLine: asValue(SalesQuoteLine),
+    SalesNote: asValue(SalesNote),
+    SalesDocumentAddress: asValue(SalesDocumentAddress),
+    SalesDocumentTag: asValue(SalesDocumentTag),
+    SalesShippingMethod: asValue(SalesShippingMethod),
+    SalesDeliveryWindow: asValue(SalesDeliveryWindow),
+    SalesPaymentMethod: asValue(SalesPaymentMethod),
+    SalesTaxRate: asValue(SalesTaxRate),
   })
 }
