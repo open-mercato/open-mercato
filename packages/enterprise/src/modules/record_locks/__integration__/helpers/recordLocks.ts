0ac63833a052144959d3d254ea132334c9e94647
Piotr Karwatka <piotr.karwatka@gmail.com>
2026-08-02 00:49:27 +0000

    test(record-locks): seed valid sales orders

--- packages/enterprise/src/modules/record_locks/__integration__/helpers/recordLocks.ts
+++ packages/enterprise/src/modules/record_locks/__integration__/helpers/recordLocks.ts
@@ -920,7 +920,16 @@ export async function createOrderFixture(
   token: string,
   currencyCode = 'USD',
 ): Promise<string> {
-  return createSalesEntity(request, token, '/api/sales/orders', { currencyCode }, ['id', 'orderId']);
+  return createSalesEntity(request, token, '/api/sales/orders', {
+    currencyCode,
+    lines: [{
+      currencyCode,
+      quantity: 1,
+      name: 'Required line',
+      unitPriceNet: 10,
+      unitPriceGross: 10,
+    }],
+  }, ['id', 'orderId']);
 }
 
 export async function createQuoteFixture(
